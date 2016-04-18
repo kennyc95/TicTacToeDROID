7e1b8ad07b308312aa77249401033f5e5358b504
kenny <kennychen0801@gmail.com>
2016-04-17 18:58:18 -0700

    player click

--- app/src/main/java/Kenny/tictactoe/MainActivity.java
+++ app/src/main/java/Kenny/tictactoe/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void beginGame(){
-
         reset.setText("Reset");
 
     }
@@ -65,5 +64,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return win;
     }
+    buttonA1.
+
 
 }
