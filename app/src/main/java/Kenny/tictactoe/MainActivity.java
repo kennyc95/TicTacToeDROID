adeb2a5c0f3f97af9f2e51fc71e4b46dc97a319b
kenny <kennychen0801@gmail.com>
2016-04-18 19:38:37 -0700

    Testing playerClick

--- app/src/main/java/Kenny/tictactoe/MainActivity.java
+++ app/src/main/java/Kenny/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package kenny.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -64,7 +65,10 @@ public class MainActivity extends AppCompatActivity {
         }
         return win;
     }
-    buttonA1.
+    public void playerClick(View v){
+        Button button = (Button) v;
+        ((Button) v).setText("x");
+    }
 
 
 }
